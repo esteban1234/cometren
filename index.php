a669b795f36bee08bd70f5ae47962cbb61f2269a
esteban1234 <tva_libra@hotmail.com>
2017-02-14 18:09:25 -0600

    inicio de la pagina

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<?php include 'vistas/encabezado.php' ?>
+<body>
+
+<article class="cabeza">
+	<article class="contenheader">
+		<div class="logo">
+			<img src="img/cometren.png" class="img-responsive" width="247" height="70" alt="cometren">
+		</div>
+		<div class="textoheader">
+			OBRA CIVIL, CONSTRUCCIONES <br> ASESORIAS, VENTAS DE MATERIAL
+		</div>
+	</article>
+</article>
+
+<div class="menu_bar">
+	<a href="#" class="bt-menu"><span class="icon-list2"></span>Menú</a>
+</div>
+ 		<div class="menu">
+			<nav>
+				<ul>
+					<li class="select"><a href="#"><span><i class="fa fa-home" aria-hidden="true"></i></span>Inicio</a></li>
+					<li><a href="#"><span><i class="fa fa-suitcase" aria-hidden="true"></i></span>Quienes somos</a></li>
+					<li><a href="#"><span><i class="fa fa-users" aria-hidden="true"></i></span>Servicios</a></li>
+					<li><a href="#"><span><i class="fa fa-phone" aria-hidden="true"></i></span>Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+
+
+
+
+
+<!-- <div class="contededortodo"> -->
+	<div class="container">
+        
+        <div class="col-md-12">
+            <div id="carousel_1" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    
+                    <!--Barra de indicadores-->
+                    <li data-target="#carousel_1" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel_1" data-slide-to="1" ></li>
+                    <li data-target="#carousel_1" data-slide-to="2"></li>
+                    <li data-target="#carousel_1" data-slide-to="3"></li>
+                    
+                </ol>
+                
+                <!--capa para contener los slide-->
+                <div class="carousel-inner" role="listbox"> 
+                    <!--la clase "carousel-inner" le indica 
+                    al navegador que el div contiene un slide, y con el atributo (role="listbox") 
+                    le decimos que este slide seguira una secuencia tipo lista--> 
+                    
+                    <div class="item active"> <!--El primer item debera contener la clase "active para 
+                                                que podamos visualizarlo"-->
+                        
+                        <img src="img/construccionc.jpg" class=" img-responsive"> <!--añadimos la clase "img-responsive" para asegurar 
+                                                                                        la correcta visualizacion en cualquier dispositivo-->
+                        <div class="carousel-caption hidden-xs"> <!--Esta capa nos sirve para agregar texto, 
+                                                                    se mostrara por encima de las imagenes con 
+                                                                    "hidden-xs" lo ocultamos en pantallas pequeñas-->
+                            <h4 class="tit_slide">Los mejores en construcción</h4>
+                            <!-- <p>Casas y edificios</p> -->
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="item">
+                        
+                        <img src="img/construccion2c.jpg" class=" img-responsive">
+                        
+                        <div class="carousel-caption hidden-xs">
+                            <h4 class="tit_slide">Construcción de casas y edificios</h4>
+                            <!-- <p>Texto de prueba numero 2</p> -->
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="item">
+                        
+                        <img src="img/construccion3c.jpg" class=" img-responsive">
+                        
+                        <div class="carousel-caption hidden-xs">
+                            <h4 class="tit_slide">El mejor equipo para construir</h4>
+                            <!-- <p>Texto de prueba numero 3</p> -->
+                        </div>
+                        
+                    </div>
+
+                    <div class="item">
+                        
+                        <img src="img/material2c.jpg" class=" img-responsive">
+                        
+                        <div class="carousel-caption hidden-xs">
+                            <h4 class="tit_slide">Materiales para la construcción</h4>
+                            <!-- <p>Texto de prueba numero 3</p> -->
+                        </div>
+                        
+                    </div>
+                    
+                    <!--Elementos de control (Prev & Next)-->
+                    <a class="left carousel-control " href="#carousel_1" role="button" data-slide="prev">
+                    	 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    					 <span class="sr-only">Previous</span>
+                    </a>
+                    
+                    <a class="right carousel-control " href="#carousel_1" role="button" data-slide="next">
+                    	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    					<span class="sr-only">Next</span>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>	
+        
+    </div>
+<!-- </div> -->
+
+<div class="primeraseccion">
+	<div class="container margen">
+		<div class="row">
+	       <div class="row">
+			<article class="col-md-6">
+	    			<figure class="col-lg-12">
+					<a href=""><img class="img img-responsive article-img" src="http://farm1.staticflickr.com/258/18511405422_d7c67c0ff8_k.jpg"></a>
+					<figcaption class="article-caption"><h1 class="article-title"><a href="">LAS CONSTRUCCIONES QUE TÚ QUIERAS</a></h1>
+					</figcaption>
+					</figure>
+					<div class="article-intro col-lg-12" style="padding-top: 10px;">
+					<strong>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor..</strong>
+					</div>
+					
+			</article>
+	                
+	        <article class="col-md-6">
+	        		<figure class="col-lg-12">
+					<a href=""><img class="img img-responsive article-img" src="img/ejemplo3.jpg"></a>
+					<figcaption class="article-caption"><h1 class="article-title"><a href="">LOS ACABADOS QUE TÚ QUIERAS</a></h1>
+					</figcaption>
+					</figure>
+	
+					<div class="article-intro col-lg-12" style="padding-top: 10px;">
+					<strong>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor..</strong>
+					</div>
+					
+			</article>
+	    </div>
+	                
+	                <hr>
+	                
+	     <div class="row">
+	        <article class="col-md-6">
+	        		<figure class="col-lg-12">
+					<a href=""><img class="img img-responsive article-img" src="http://farm1.staticflickr.com/510/18737505996_98a20bf592_k.jpg"></a>
+					<figcaption class="article-caption"><h1 class="article-title"><a href="">LA CASA QUE TÚ QUIERAS</a></h1>
+					</figcaption>
+					</figure>
+					<div class="article-intro col-lg-12" style="padding-top: 10px;">
+					<strong>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor..</strong>
+					</div>
+					
+			</article>
+	                
+	        <article class="col-md-6">
+	        		<figure class="col-lg-12">
+					<a href=""><img class="img img-responsive article-img" src="http://farm1.staticflickr.com/346/18328027128_704280cd4c_k.jpg"></a>
+					<figcaption class="article-caption"><h1 class="article-title"><a href="">LOS MATERIALES QUE TU QUIERAS</a></h1>
+					</figcaption>
+					</figure>
+	
+					<div class="article-intro col-lg-12" style="padding-top: 10px;">
+					<strong>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor..</strong>
+					</div>
+					
+			</article>
+	      </div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.js"></script>
+<script type="text/javascript" src="js/iconoarriba.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/menu.js"></script>
+</body>
+</html>
---
+++ vistas/envia.php
@@ -0,0 +1,48 @@
+<?php
+//Librerías para el envío de mail
+require('../phpmailer/phpmailer/class.phpmailer.php');
+//PHPMailer Object
+$mail = new PHPMailer();
+
+// Recoger los valores del Formulario
+
+$nombre = $_POST['name'];
+$telefono = $_POST['phone'];
+$correo = $_POST['email'];
+$comentario = $_POST['message'];
+
+$mail->CharSet = 'utf-8';
+//From email address and name
+$mail->From = $correo;
+$mail->FromName = $nombre;
+
+//To address and name
+$mail->addAddress("info@corporativocaos.com");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Cotizacion Caos";
+$mail->Body = "
+              <b>Telefono: </b>.$telefono.<br/>
+              <b>Comentario: </b>.$comentario.<br/>";
+// $mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send())
+{
+    // echo "Error al enviar Mensaje: " . $mail->ErrorInfo;
+    echo ("<SCRIPT LANGUAGE='JavaScript'>
+          window.alert('Error al Enviar MENSAJE')
+          window.location.href='contacto.php'
+        </SCRIPT>");
+
+}
+else
+{
+  echo ("<SCRIPT LANGUAGE='JavaScript'>
+        window.alert('Mensaje Enviado Correctamente')
+        window.location.href='contacto.php'
+      </SCRIPT>");
+
+
+}
+?>
